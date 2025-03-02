574a43893f0560773ae1364b1b96e2f54089e991
alula-z <azeruesenay14@gmail.com>
2025-03-02 16:41:13 -0500

    Added basic structure of module

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,14 @@
+export default function login() {
+    return (
+        <div id='wrapper' className='flex flex-col h-full w-full justify-center'>
+            
+            <div id = 'Class1 Module' className= 'flex flex-col justify-center p-2'>
+                <div id ='Class1-title-section' className = 'flex items-center justify-center border-2 border-black border-solid p-2 max-w-xs h-15 relative'>
+                    <h1 id = 'Class1-name' className='flex items-center justify-center max-w-xs h-15'>Class Name</h1>
+                    <h1 id = 'Class1-grade' className= 'top-right absolute top-1 right-2 text-sm'>100.00% - A</h1>
+                </div>
+                <h1 id = 'class-Body' className ='flex items-center border-2 border-black border-solid p-2 max-w-xs h-25'>Class Body here</h1>
+            </div>
+
+        </div>
+    )}
