49f0714fbd2c9a22ce98e7b0a6409f4c8ec5fa13
PRAjwal03714 <vprajwal714@gmail.com>
2025-04-24 21:59:46 -0400

    Deployed backend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,14 @@
+console.log("💥 ENV @ build:", process.env.NEXT_PUBLIC_API_URL);
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
   typescript: {
     ignoreBuildErrors: process.env.NEXT_IGNORE_TYPE_ERRORS === 'true',
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
   reactStrictMode: true,
 };
