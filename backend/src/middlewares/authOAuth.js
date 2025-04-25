166ae15a1aa61c182aeb680a249cc01aeb18c5dc
PRAjwal03714 <vprajwal714@gmail.com>
2025-04-25 12:55:01 -0400

    completed deployment

--- backend/src/middlewares/authOAuth.js
+++ backend/src/middlewares/authOAuth.js
@@ -8,7 +8,7 @@ const pool = require("../config/db");
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "/api/auth/google/callback"
+    callbackURL: "https://lms-backend-38al.onrender.com/api/auth/google/callback"
 }, async (accessToken, refreshToken, profile, done) => {
     try {
         const email = profile.emails[0].value;
